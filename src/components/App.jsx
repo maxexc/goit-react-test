ef22cf4c8cecc8b7b98c79de402a6372e05afcbd
Maksym Solomko <max.exc7@gmail.com>
2022-12-01 21:54:04 +0200

    Dropdown

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Counter from './Counter/Counter';
+import Dropdown from './Dropdown/Dropdown'
 
 export const App = () => (
   <>
     <h1>Состояние компонента</h1>
     <Counter />
+    <Dropdown />
   </>
 )
 
---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './Dropdown.css'
+
+
+class Dropdown extends Component {
+    state = {
+        visible: false,
+    };
+
+    show = () => {
+        this.setState({ visible: true });
+    }
+
+    hide = () => {
+        this.setState({ visible: false });
+    }
+
+
+    render() {
+        return ( 
+            <div className='Dropdown'>
+                <button type="button" className='Dropdown__toggle' onClick={ this.show} >
+                Показать
+                </button>
+
+                <button type="button" className='Dropdown__toggle' onClick={ this.hide} >
+                Скрыть
+                </button>
+
+                <div className="Dropdown__menu">Выпадающее меню</div>
+            </div>
+        )
+    }
+}
+
+export default Dropdown
