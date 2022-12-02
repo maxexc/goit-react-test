35321995452d7e28798ba263e10f49b917aabe02
Maksym Solomko <max.exc7@gmail.com>
2022-12-02 21:50:39 +0200

    Update ColorPicker.jsx

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -3,29 +3,40 @@ import './ColorPicker.css'
 
 class ColorPicker extends React.Component {
     state = {
-        activeOptionIdx: 3,
+        activeOptionIdx: 1,
+    };
+
+    setActiveIdx = (index) => {
+        this.setState({activeOptionIdx: index})
+
+    }
+
+    makeOptionClassName = index => {
+        const optionClasses = ['ColorPicker__option'];
+
+        if(index === this.state.activeOptionIdx) {
+            optionClasses.push('ColorPicker__option--active')
+           }
+        return optionClasses.join(' ');
     };
      
     render() {
+        const {label} = this.props.options[this.state.activeOptionIdx]
+        console.log({label})
+
         return (
             <div className="ColorPicker">
                 <h2 className="ColorPicker__title">Color Picker</h2>
+                <p>Выбран цвет: {label}</p>
                 <div>
-                    {this.props.options.map(({ label, color }, index) => {
-                        const optionClasses = ['ColorPicker__option'];
-
-                        if(index === this.state.activeOptionIdx) {
-                            optionClasses.push('ColorPicker__option--active')
-                        }
-                        return (
-                            <button
+                    {this.props.options.map(({ label, color }, index) => (
+                        <button
                             key={label}
-                            className={optionClasses.join(' ')}
+                            className={this.makeOptionClassName(index)}
                             style={{ backgroundColor: color }}
-                            //   onClick={() => this.setActiveIdx(index)}
-                            ></button>
-                        );
-                    })}                    
+                            onClick={() => this.setActiveIdx(index) }
+                        ></button>
+                    ))}                     
                 </div>
             </div>
         )
