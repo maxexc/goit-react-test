be30b220fcb814b9f4a0e8db2363684d485199cd
Maksym Solomko <max.exc7@gmail.com>
2022-12-02 18:14:54 +0200

    Update ColorPicker.jsx

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import './ColorPicker.css'
 
 class ColorPicker extends React.Component {
+    state = {
+        activeOptionIdx: 0,
+    };
+    
     render() {
         return (
             <div className="ColorPicker">
                 <h2 className="ColorPicker__title">Color Picker</h2>
                 <div>
                     {this.props.options.map(({ label, color }) => (
-                        <span
+                        <button
                             key={label}
                             className='ColorPicker__option'
                             style={{ backgroundColor: color }}
                             //   onClick={() => this.setActiveIdx(index)}
-                        ></span>
+                        ></button>
                     ))}                    
                 </div>
             </div>
